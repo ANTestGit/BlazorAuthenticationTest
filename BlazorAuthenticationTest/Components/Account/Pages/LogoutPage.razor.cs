4884527b4ff44564be81ce470b8cc4349b244ab2
AlexNek <alexnek@russinger.com>
2024-08-11 14:21:43 +0200

    all logout and user info

---
+++ BlazorAuthenticationTest/Components/Account/Pages/LogoutPage.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorAuthenticationTest.Components.Account.Pages
+{
+    public partial class LogoutPage : ComponentBase
+    {
+
+    }
+}
